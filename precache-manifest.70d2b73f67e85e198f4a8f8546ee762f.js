98bedaa84972a7114b17de6766242a890ac4d23f
ladymelven <ladymelven@yandex.ru>
2020-07-17 18:49:39 +0300

    Updates

---
+++ precache-manifest.70d2b73f67e85e198f4a8f8546ee762f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399d8d35f37ff99f9928298380a2ac9b",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "723a265fce612a39cbe7",
+    "url": "/burger-builder/static/css/main.b9a87c93.chunk.css"
+  },
+  {
+    "revision": "736049684dc6c0b1a079",
+    "url": "/burger-builder/static/js/2.bf9e3f4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.bf9e3f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723a265fce612a39cbe7",
+    "url": "/burger-builder/static/js/main.3d95c0b3.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  }
+]);
